14a674009d47cace1f1813b8ec0aadd5e2883804
josh.pencheon <josh.pencheon@153ec483-9fa4-4787-9925-b21d4235b228>
2020-01-28 18:05:15 +0000

    # cleanup commented code
    git-svn-id: https://deepthought:4142/svn/non-era/gems/ndr_browser_timings@21331 153ec483-9fa4-4787-9925-b21d4235b228

--- app/models/ndr_browser_timings/resource_timing.rb
+++ app/models/ndr_browser_timings/resource_timing.rb
@@ -1,5 +1,8 @@
 module NdrBrowserTimings
   # Wraps window.performance.getEntry() objects
+  #
+  # These are similar to, but not the same as, the data structure
+  # returned from the primary navigation.
   class ResourceTiming < PerformanceTiming
     include ActiveModel::Model
 
@@ -13,29 +16,5 @@ module NdrBrowserTimings
       attr_accessor field.underscore
       alias_attribute field, field.underscore if field != field.underscore
     end
-
-    # def tags
-    #   { controller: controller, action: action }
-    # end
-
-    # def recordable?
-    #   tags.values.all?(&:present?)
-    # end
-
-    # def timeline
-    #   @timeline ||= {
-    #     unload: unload_event_end - unload_event_start,
-    #     redirect: redirect_end - redirect_start,
-    #     dns: domain_lookup_end - domain_lookup_start,
-    #     connect: connect_end - connect_start,
-    #     request: response_start - request_start,
-    #     response: response_end - response_start,
-    #     dom_loading: dom_loading - response_start,
-    #     dom_interactive: dom_interactive - dom_loading,
-    #     dom_content_loaded_event: dom_content_loaded_event_end - dom_content_loaded_event_start,
-    #     dom_complete: dom_complete - dom_content_loaded_event_end,
-    #     load_event: load_event_end - load_event_start
-    #   }
-    # end
   end
 end
